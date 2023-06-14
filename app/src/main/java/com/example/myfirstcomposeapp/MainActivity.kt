3fdefeb8eef40e8df6ed698b6c200103c89a715e
farida <farida.hairullina5@gmail.com>
2023-06-14 18:12:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.example.myfirstcomposeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myfirstcomposeapp.ui.theme.MyFirstComposeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyFirstComposeAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText(stringResource(R.string.title), stringResource(R.string.text_one), stringResource(R.string.text_two))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(message: String, textOne: String, textTwo: String,modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+    ) {
+        val image = painterResource(id = R.drawable.bg_compose_background)
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Text(
+            text = message,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = textOne,
+            modifier = Modifier
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp
+                ),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = textTwo,
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+
+@Preview(
+    showBackground = true,
+    showSystemUi = false
+)
+@Composable
+fun MyFirstComposeAppPreview() {
+    MyFirstComposeAppTheme {
+        GreetingText(message = stringResource(R.string.title), stringResource(R.string.text_one), stringResource(
+            id = R.string.text_two
+        ))
+    }
+}
