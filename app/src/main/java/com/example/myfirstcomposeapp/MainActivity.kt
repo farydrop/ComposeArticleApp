7c5808b06f66167c9a0926d0e964d77edb6516ee
farida <farida.hairullina5@gmail.com>
2023-06-14 18:54:18 +0200

    Initial

--- app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstcomposeapp/MainActivity.kt
@@ -45,7 +45,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingText(message: String, textOne: String, textTwo: String,modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier
     ) {
         val image = painterResource(id = R.drawable.bg_compose_background)
         Image(
